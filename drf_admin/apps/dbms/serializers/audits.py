bcf5b2e8f17c818dfea413f6224c8717e27fed25
郭海涵 <guohh@guoguokeji.com>
2021-09-09 15:29:49 +0800

    增加db_name返回字段

--- drf_admin/apps/dbms/serializers/audits.py
+++ drf_admin/apps/dbms/serializers/audits.py
@@ -13,9 +13,14 @@ from dbms.models import Audits
 class AuditsSerializer(serializers.ModelSerializer):
     # 添加额外字段
     db_env = serializers.SerializerMethodField(label="执行环境")
+    db_name = serializers.SerializerMethodField(label="数据库名称")
+
     class Meta:
         model = Audits
         fields = "__all__"
 
     def get_db_env(self, obj):
         return obj.db.get_db_env_display()
+
+    def get_db_name(self, obj):
+        return obj.db.db_name
